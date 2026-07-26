f814a65b1b91b7e6b3a66c17e4b7e5314473809e
Abdullin Albert <albert@MacBook-Pro-Albert.local>
2026-07-26 23:33:25 +0500

    иконки, меню задач таск-трекера

--- app/src/main/java/ru/albertabdullin/todooshka/presentation/MainActivity.kt
+++ app/src/main/java/ru/albertabdullin/todooshka/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package ru.albertabdullin.todooshka
+package ru.albertabdullin.todooshka.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.albertabdullin.todooshka.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/ru/albertabdullin/todooshka/presentation/screen/tasks/TaskContainerFragment.kt
@@ -0,0 +1,9 @@
+package ru.albertabdullin.todooshka.presentation.screen.tasks
+
+import androidx.fragment.app.Fragment
+import ru.albertabdullin.todooshka.R
+
+class TaskContainerFragment : Fragment(R.layout.task_container) {
+
+
+}
